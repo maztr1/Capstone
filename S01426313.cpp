5a05c0544f2136bf3bc80c22f5a8a550755e8f14
CSCStudent <robert.gilanyi@frontrange.edu>
2023-03-30 13:46:48 -0600

    3/30/update

--- S01426313.cpp
+++ S01426313.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-// implementation file for student S01426313
 
+// implementation file for student S01426313
+// CSC2034 Spring 2023
 namespace S01426313{
 void run() {
 	std::cout << "hello from SA01426313" << std::endl;
