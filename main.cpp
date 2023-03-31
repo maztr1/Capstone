9f6ec6c8cf82d008f86a39756768793c553d201a
CSCStudent <robert.gilanyi@frontrange.edu>
2023-03-31 15:55:00 -0600

    3/31/2023 added namespace DEMO

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "S01426313.h"
+#include "demo.h"
+
 using namespace std;
 
 // Driver for capstone project
 int main() {
 	S01426313::run();
+
+	DEMO::run();
 
 	return 0;
 }
